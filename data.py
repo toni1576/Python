8868a086a7a0e553590fce8da4878d8e41c8305b
Antonio <antoniocgstudent@gmail.com>
2026-08-26 11:53:56 -0500

    Add data.py with initial variable definitions and print statements

---
+++ data.py
@@ -0,0 +1,35 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+# String
+animal = "Pony"
+animals = "Pony's"
+
+# Int and float
+quantity = 50
+loan = 500
+single_cost = 1110.50
+
+# Boolean TvF
+animal_sale = True
+
+# print
+print("")
+print(
+    f"Hello! would you like to buy a some {animals}? we have {quantity} available for purchase."
+)
+print(
+    f"for one {animal} it will cost you ${single_cost} or we could loan you ${loan} and you can pay us later."
+)
+print("")
